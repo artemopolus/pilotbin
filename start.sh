75c43c74db4a11b99183f425479875195a97dc1b
Artem <artem.o.kuznecov@gmail.com>
2019-10-31 12:10:16 +0300

    sm fx

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#!/bin/bash
+
+
+./copilot_cross -1 200 -2 1600 -3 1400 -4 -10 -n 10007 -p "ttyS0" -b 57600 -B 57600 -c 2 -f 15001
+
+
+# ./copilot -1 200 -2 1600 -3 1400 -4 -10 -n "ttyUSB0" -p "ttyS0" -b 57600 -B 57600 -c 1 -f 15001
+#/home/pi/projects/copilot_2/cmake-build-debug/copilot -1 1 -2 1600 -3 1400 -4 -10 -n "ttyUSB0" -p "ttyS0" -b 57600 -B 57600 -c 1 -f 15001
