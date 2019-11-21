6d2c1d35d153fdd5203501a5ac43158a7731beea
Artem <artem.o.kuznecov@gmail.com>
2019-11-21 12:08:48 +0300

    fast

--- downloadFROMraspberrypi.sh
+++ downloadFROMraspberrypi.sh
@@ -6,20 +6,25 @@ else
 	mkdir log
 fi
 sshpass -p "raspberry" scp -r pi@192.168.7.1:log/ log/
+cd log
 x=0
 trg="outcpp_$x.avi"
-while [ -e $trg ]; do
-	# while [ 1 ]; do
+# while [ -e $trg ]; do
+while [ 1 ]; do
 	#statements
-	echo "$trg"
-	x=$(( $x + 1 ))
-	trg="outcpp_$x.avi"
+	# echo "check: $trg"
+	if [ -e $trg ]; then
+		#statements
+		echo "$trg is executed"
+		x=$(( $x + 1 ))
+		trg="outcpp_$x.avi"
+	else
+		break
+	fi
 	
-	# if [ $x -gt 20 ]; then
-	# 	#statements
-	# 	break
-	# fi
 done
+echo "use name $trg"
+cd ..
 sshpass -p "raspberry" scp pi@192.168.7.1:outcpp.avi log/$trg
 
 echo "done"
--- start.sh
+++ start.sh
@@ -18,5 +18,5 @@
 # ./copilot -1 200 -2 1600 -3 1400 -4 -10 -n "ttyUSB0" -p "ttyS0" -b 57600 -B 57600 -c 1 -f 15001
 #/home/pi/projects/copilot_2/cmake-build-debug/copilot -1 1 -2 1600 -3 1400 -4 -10 -n "ttyUSB0" -p "ttyS0" -b 57600 -B 57600 -c 1 -f 15001
 
-./copilot_cross -1 200 -2 1600 -3 1400 -4 -10 -n 10007 -p "ttyS0" -b 57600 -B 57600 -c 2 -f 15001
+./copilot_cross -1 200 -2 1600 -3 1400 -4 -10 -n 10007 -p "ttyS0" -b 921600 -B 57600 -c 2 -f 15001
 
